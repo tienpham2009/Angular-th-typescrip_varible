1ee2b2f295164cf5bc3153d8790690a0d4f0b1c5
tienpham2009 <tiendx2009@gmail.com>
2021-07-12 21:30:09 +0700

    typescript-promise

--- src/main.js
+++
@@ -1,6 +0,0 @@
-let width;
-let height;
-width = 10.5;
-height = 20;
-let area = width * height;
-console.log("Dien tich hinh chu nhat la : " + area);
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
-let width: number;
-let height: number;
+let money = 10000;
 
-width = 10.5;
-height = 20;
+const buyACar = (car : any )  => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>{
+        
+        if(money >= 10000)
+        {
+            resolve("can buy" + car);
+        }else{
+            reject("Do not enough money");
+        }
+    } , 100);
+  })
+}
 
-let area: number = width * height ; 
- 
-console.log(`Dien tich hinh chu nhat la : ${area}`);
-
+money = 1000001;
+const promise = buyACar("Vinfast").then(value => {
+    console.log(value);
+}, error => {
+    console.log(error);
+})
