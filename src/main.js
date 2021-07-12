b3560f54f061e18dff326e33fde8d81b1c026d2a
tienpham2009 <tiendx2009@gmail.com>
2021-07-12 13:51:25 +0700

    typescript varible

---
+++ src/main.js
@@ -0,0 +1,6 @@
+let width;
+let height;
+width = 10.5;
+height = 20;
+let area = width * height;
+console.log("Dien tich hinh chu nhat la : " + area);
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+let width: number;
+let height: number;
+
+width = 10.5;
+height = 20;
+
+let area: number = width * height ; 
+ 
+console.log(`Dien tich hinh chu nhat la : ${area}`);
+
